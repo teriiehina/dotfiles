15128a0e5a2220a6ccdd03ba7e8be30a7dedde84
Peter Meuel <pmeuel.externe@pagesjaunes.fr>
2013-12-04 13:05:22 +0100

    Copy bépo keyboard layout as part of the install process

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 # Oh-my-zsh
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+
+# Install the bépo keyboard layout
+
+sudo cp fr-dvorak-bepo.bundle /Library/Keyboard\ Layouts/fr-dvorak-bepo.bundle
