c5d0be46b23579bda5150a6d610af3ef7cf633d8
teriiehina <peter@teriiehina.net>
2014-01-24 13:16:01 +0100

    Update print-escpos.sh

--- print-escpos.sh
+++ print-escpos.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-brew install freetype
-ln -s /usr/local/include/freetype2 /usr/local/include/freetype
-
 brew install libusb
+brew install freetype
+
+ln -s /usr/local/include/freetype2 /usr/local/include/freetype
 
 sudo pip install PIL
 sudo pip install qrcode
 sudo pip install pyusb==1.0.0b1
 sudo pip install pyserial
+sudo pip install SOAPpy
+sudo pip install suds
