302993ab91f3516fb99b7fa3f952bdf235d51035
teriiehina <peter@teriiehina.net>
2014-01-24 13:03:53 +0100

    Update print-escpos.py

--- print-escpos.py
+++ print-escpos.py
@@ -1,4 +1,4 @@
 # installer freetype via brew
-# ln -s /usr/local/Cellar/freetype/2.5.1/include/freetype2 /usr/local/include/freetype
+# ln -s /usr/local/include/freetype2 /usr/local/include/freetype
 
 # installer python image library
